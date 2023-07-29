25d258f0a718b8947328ae639e516a9ae56bfd44
seminchoi <choicco89@gmail.com>
2023-07-29 12:02:51 +0900

    init: 프로젝트 생성 Java 17, Spring Boot 3.1.2

---
+++ src/main/java/smu/likelion/jikchon/JikchonApplication.java
@@ -0,0 +1,13 @@
+package smu.likelion.jikchon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JikchonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JikchonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smu/likelion/jikchon/JikchonApplicationTests.java
@@ -0,0 +1,13 @@
+package smu.likelion.jikchon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JikchonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
