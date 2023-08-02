a7b4abc20c6719399bb491fb6b2a8cf5de0ae107
chaerish <ccyy8432@naver.com>
2023-08-03 00:48:04 +0900

    cart service 틀만 잡음

--- src/main/java/smu/likelion/jikchon/service/CartService.java
+++ src/main/java/smu/likelion/jikchon/service/CartService.java
@@ -5,6 +5,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import smu.likelion.jikchon.base.PageResult;
+import smu.likelion.jikchon.dto.cart.CartProductReturnDto;
 import smu.likelion.jikchon.repository.CartRepository;
 import smu.likelion.jikchon.repository.MemberRepository;
 
@@ -16,9 +18,20 @@ public class CartService {
     private final CartRepository cartRepository;
     private final MemberRepository memberRepository;
 
-    //유저 장바구니 조회
-    public Page
+    //유저의 장바구니 조회 - 페이징이용
+    public PageResult<CartProductReturnDto> getMemberCartList(){
+        return null;
+    }
 
+    //유저가 프로덕트를 장바구니에 추가
+    public void addProductToCart(Long productId,Long quantity){
+
+    }
+
+    //유저가 장바구니에서 프로덕트를 삭제
+    public void deleteProductInCart(Long productId){
+
+    }
 
 
 }
