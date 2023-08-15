034d0d7668b00881cc2fa2773d3d4684d85d38d3
seminchoi <choicco89@gmail.com>
2023-08-15 21:42:00 +0900

    fix : 오류 수정

--- src/main/java/smu/likelion/jikchon/service/S3ImageService.java
+++ src/main/java/smu/likelion/jikchon/service/S3ImageService.java
@@ -1,37 +1,31 @@
 package smu.likelion.jikchon.service;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import smu.likelion.jikchon.domain.Image;
 import smu.likelion.jikchon.domain.Product;
+import smu.likelion.jikchon.domain.ProductImage;
 import smu.likelion.jikchon.domain.enumurate.Target;
-import smu.likelion.jikchon.exception.CustomBadRequestException;
-import smu.likelion.jikchon.exception.CustomException;
-import smu.likelion.jikchon.exception.ErrorCode;
 import smu.likelion.jikchon.repository.ImageRepository;
 import smu.likelion.jikchon.s3.S3Uploader;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 @Service
+@Primary
 @RequiredArgsConstructor
-public class S3ImageService implements ImageService{
+public class S3ImageService implements ImageService {
     private final ImageRepository imageRepository;
     private final S3Uploader s3Uploader;
 
+
     @Override
-    public void saveImageList(Long targetId, Target target, List<MultipartFile> imageList) {
-        if (!imageList.isEmpty()) {
-            List<String> urlList = s3Uploader.s3MultipleUploadOfFileNullSafe(Target.PRODUCT, imageList);
-            for (String url : urlList) {
-                    imageRepository.save(Image.builder()
-                            .imageUrl(url)
-                            .target(target)
-                            .targetId(targetId)
-                            .build());
+    public void saveProductImageList(Product product, List<MultipartFile> imageList) {
+        if (imageList != null) {
+            List<String> imageUrlList = s3Uploader.s3MultipleUploadOfFileNullSafe("product", imageList);
+            for (String imageUrl : imageUrlList) {
+                imageRepository.save(new ProductImage(product, imageUrl));
             }
         }
     }
