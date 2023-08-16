c734d2076f1acea110d332f71d74e0b7550c74ce
seminchoi <choicco89@gmail.com>
2023-08-16 22:02:31 +0900

    feat : 부모 카테고리로도 검색이 가능하도록 기능 추가

--- src/main/java/smu/likelion/jikchon/domain/enumurate/Category.java
+++ src/main/java/smu/likelion/jikchon/domain/enumurate/Category.java
@@ -1,8 +1,12 @@
 package smu.likelion.jikchon.domain.enumurate;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import smu.likelion.jikchon.exception.CustomBadRequestException;
+import smu.likelion.jikchon.exception.ErrorCode;
 
 @AllArgsConstructor
+@Getter
 public enum Category {
     PRODUCE(1, "농산물"),
 
@@ -12,6 +16,16 @@ public enum Category {
 
     PROCESSED_FOODS(4, "가공식품");
 
+
     private final int id;
     private final String description;
+
+    public static Category fromId(int value) {
+        for (Category category : Category.values()) {
+            if (category.getId() == value) {
+                return category;
+            }
+        }
+        throw new CustomBadRequestException(ErrorCode.INVALID_PARAMETER);
+    }
 }
