a15bec1cf1b5e4f949dc653d3c84742ff0cb36a3
jiwonsudo <wondev112@gmail.com>
2023-08-17 21:50:59 +0900

    hotfix: login fix

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,4 +1,4 @@
-import { checkTokenExistence } from "./common/jwt_token_check";
+import { checkTokenExistence } from "./common/jwt_token_check.js";
 
 const REGEX_PHONENUMBER = /^01([0|1|6|7|8|9])-?([0-9]{4})-?([0-9]{4})$/; // 앞자리가 01이며 (0,1,6,7,8,9) 이며 중간에 4자리, 세번째는 4자리인 전화번호
 const REGEX_PASSWORD = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; // 영어와 숫자를 포함한 8자리 이상의 비밀번호
